bffb9386a14a380679f4bfcc1252cba5a934667b
Alp Toker <tokeralp@gmail.com>
2022-07-05 10:22:48 +0300

    testing

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import React, { useState, useEffect } from "react";
 import _ from "lodash";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Home from "./Home";
+import Hometwo from "./Hometwo";
 import LikedImages from "./LikedImages";
 
 function App() {
   return (
     <Routes>
-      <Route exact path="/" element={<Home />} />
+      <Route exact path="/" element={<Hometwo />} />
       <Route exact path="/liked_images" element={<LikedImages />} />
     </Routes>
   );
